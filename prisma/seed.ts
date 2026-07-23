42e7176cfdb2e9baefb38059b4b6584944095b0c
denzubechi <chukwumasamuel371@gmail.com>
2026-07-23 19:22:32 +0100

    doot clone

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,108 +3,60 @@
 // const prisma = new PrismaClient();
 
 // async function main() {
-//   const customerUpdates = [
-//     {
-//       key: "customers_app_update1",
-//       isUpdateAvailable: true,
-//       mustUpdate: true,
-//       androidAppLink:
-//         "https://play.google.com/store/apps/details?id=com.chariotconnect.customer",
-//       iosAppLink: "https://apps.apple.com/app/id0000000001",
-//       isAndroid: true,
-//       isIos: true,
-//     },
-//     {
-//       key: "customers_app_update2",
-//       isUpdateAvailable: true,
-//       mustUpdate: false,
-//       androidAppLink:
-//         "https://play.google.com/store/apps/details?id=com.chariotconnect.customer",
-//       iosAppLink: "https://apps.apple.com/app/id0000000001",
-//       isAndroid: true,
-//       isIos: true,
-//     },
-//     {
-//       key: "customers_app_update3",
-//       isUpdateAvailable: false,
-//       mustUpdate: false,
-//       androidAppLink:
-//         "https://play.google.com/store/apps/details?id=com.chariotconnect.customer",
-//       iosAppLink: "https://apps.apple.com/app/id0000000001",
-//       isAndroid: true,
-//       isIos: true,
-//     },
-//   ];
+//   await prisma.customerAppUpdate.deleteMany({});
+//   await prisma.riderAppUpdate.deleteMany({});
+//   await prisma.vendorAppUpdate.deleteMany({});
 
-//   const riderUpdates = [
-//     {
-//       key: "riders_app_update1",
-//       isUpdateAvailable: true,
-//       mustUpdate: true,
-//       androidAppLink:
-//         "https://play.google.com/store/apps/details?id=com.chariotconnect.rider",
-//       iosAppLink: "https://apps.apple.com/app/id0000000002",
-//       isAndroid: true,
-//       isIos: true,
-//     },
-//     {
-//       key: "riders_app_update2",
-//       isUpdateAvailable: false,
-//       mustUpdate: false,
-//       androidAppLink:
-//         "https://play.google.com/store/apps/details?id=com.chariotconnect.rider",
-//       iosAppLink: "https://apps.apple.com/app/id0000000002",
-//       isAndroid: true,
-//       isIos: true,
-//     },
-//   ];
+//   const customerUpdate = {
+//     key: "customers_app_update1",
+//     isUpdateAvailable: true,
+//     mustUpdate: false,
+//     androidAppLink:
+//       "https://play.google.com/store/apps/details?id=com.chariotconnect.customer",
+//     iosAppLink: "https://apps.apple.com/app/id0000000001",
+//     isAndroid: true,
+//     isIos: true,
+//   };
 
-//   const vendorUpdates = [
-//     {
-//       key: "vendors_app_update1",
-//       isUpdateAvailable: true,
-//       mustUpdate: true,
-//       androidAppLink:
-//         "https://play.google.com/store/apps/details?id=com.chariotconnect.vendor",
-//       iosAppLink: "https://apps.apple.com/app/id0000000003",
-//       isAndroid: true,
-//       isIos: true,
-//     },
-//     {
-//       key: "vendors_app_update2",
-//       isUpdateAvailable: false,
-//       mustUpdate: false,
-//       androidAppLink:
-//         "https://play.google.com/store/apps/details?id=com.chariotconnect.vendor",
-//       iosAppLink: "https://apps.apple.com/app/id0000000003",
-//       isAndroid: true,
-//       isIos: true,
-//     },
-//   ];
+//   const riderUpdate = {
+//     key: "riders_app_update1",
+//     isUpdateAvailable: true,
+//     mustUpdate: false,
+//     androidAppLink:
+//       "https://play.google.com/store/apps/details?id=com.chariotconnect.rider",
+//     iosAppLink: "https://apps.apple.com/app/id0000000002",
+//     isAndroid: true,
+//     isIos: true,
+//   };
 
-//   for (const data of customerUpdates) {
-//     await prisma.customerAppUpdate.upsert({
-//       where: { key: data.key },
-//       update: data,
-//       create: data,
-//     });
-//   }
+//   const vendorUpdate = {
+//     key: "vendors_app_update1",
+//     isUpdateAvailable: true,
+//     mustUpdate: false,
+//     androidAppLink:
+//       "https://play.google.com/store/apps/details?id=com.chariotconnect.vendor",
+//     iosAppLink: "https://apps.apple.com/app/id0000000003",
+//     isAndroid: true,
+//     isIos: true,
+//   };
 
-//   for (const data of riderUpdates) {
-//     await prisma.riderAppUpdate.upsert({
-//       where: { key: data.key },
-//       update: data,
-//       create: data,
-//     });
-//   }
+//   await prisma.customerAppUpdate.upsert({
+//     where: { key: customerUpdate.key },
+//     update: customerUpdate,
+//     create: customerUpdate,
+//   });
 
-//   for (const data of vendorUpdates) {
-//     await prisma.vendorAppUpdate.upsert({
-//       where: { key: data.key },
-//       update: data,
-//       create: data,
-//     });
-//   }
+//   await prisma.riderAppUpdate.upsert({
+//     where: { key: riderUpdate.key },
+//     update: riderUpdate,
+//     create: riderUpdate,
+//   });
+
+//   await prisma.vendorAppUpdate.upsert({
+//     where: { key: vendorUpdate.key },
+//     update: vendorUpdate,
+//     create: vendorUpdate,
+//   });
 // }
 
 // main()
